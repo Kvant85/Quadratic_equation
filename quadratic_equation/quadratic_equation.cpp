1f4217c2c24de066b6ef00e5d54db360e7487a42
Kvant85 <Kvant85@mail.ru>
2024-05-13 10:13:30 +0300

    Add files via upload

--- quadratic_equation/quadratic_equation.cpp
+++ quadratic_equation/quadratic_equation.cpp
@@ -1,44 +1,43 @@
 #include <stdio.h>
 #include <math.h>
-#include <iostream>
 
 struct quadratic_equation
 {
 	bool haveRealResult;				//Есть ли вещественный результат решения
-	double firstResult, secondResult;	//Первый и второй результаты
+	long double firstResult, secondResult;	//Первый и второй результаты
 };
 
 quadratic_equation solve_equation(double _a, double _b, double _c)
 {
 	//Возвращаемая структура с результатом вычисления
-	quadratic_equation res;
+	quadratic_equation result;
 
 	if (_a == 0 && _b !=0)	//Не квадратное уравнение вида bX + c = 0, где a = 0
 	{
-		res.haveRealResult = true;
-		res.firstResult = (0 - _c) / _b;
-		res.secondResult = res.firstResult;
-		return res;
+		result.haveRealResult = true;
+		result.firstResult = (0 - _c) / _b;
+		result.secondResult = result.firstResult;
+		return result;
 	}
 	else if (_a == 0 && _b == 0)	//Уравнение вида 0*X^2 + 0*X + c = 0, не имеющее решения
 	{
-		res.haveRealResult = false;
-		return res;
+		result.haveRealResult = false;
+		return result;
 	}
 
 	//Дискриминант b^2 - 4*a*c:
 	long double disc = _b * _b - 4 * _a * _c;
 
 	//Нет вещественных результатов - дискриминант меньше нуля
-	if (disc < 0) res.haveRealResult = false;
+	if (disc < 0) result.haveRealResult = false;
 	//1 или 2 результата - дискриминант больше или равен нулю
 	else
 	{
-		res.haveRealResult = true;
-		res.firstResult = (sqrt(disc) - _b) / (2 * _a);
-		res.secondResult = (0 - sqrt(disc) - _b) / (2 * _a);
+		result.haveRealResult = true;
+		result.firstResult = (sqrt(disc) - _b) / (2 * _a);
+		result.secondResult = (0 - sqrt(disc) - _b) / (2 * _a);
 	}
-	return res;
+	return result;
 }
 
 int main()
@@ -49,5 +48,4 @@ int main()
 
 	if (equation.haveRealResult) printf("Results: %g; %g", equation.firstResult, equation.secondResult);
 	else printf("No roots");
-	std::cout << std::endl;
 }
